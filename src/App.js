0c2caf0974be71b9f05140882b5b4ce3123f3aa2
Casey MacLeod <caseymacleod22@gmail.com>
2022-01-10 10:35:42 -0500

    adds ThirdRow component and table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import TopRow from "./components/TopRow";
 import SecondRow from "./components/SecondRow";
+import ThirdRow from "./components/ThirdRow";
 
 function App() {
   return (
     <div>
         <TopRow />
         <SecondRow />
+        <ThirdRow />
     </div>
   )
 }
