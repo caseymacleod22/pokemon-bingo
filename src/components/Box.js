dc60088368f64cfe07596608bbfd2e56422c2515
Casey MacLeod <caseymacleod22@gmail.com>
2022-01-10 12:02:01 -0500

    adds name to placeholder data

--- src/components/Box.js
+++ src/components/Box.js
@@ -7,15 +7,25 @@ const Container = styled.td`
 `
 
 const Image = styled.img`
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 70%;
+    height: 70%;
+`
+
+const Name = styled.p`
+    color: blue;
+    font-size: 10px;
+    text-align: center;
+    padding-bottom: 0;
 `
 
 const Box = ({item}) => {
     return (
         <Container>
             <Image src={item.img} />
+            <Name>{item.name}</Name>
         </Container>
     )
 }
