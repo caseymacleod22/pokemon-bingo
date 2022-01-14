de9050b0eb9019189848dd14a48c149d9d29b560
Casey MacLeod <caseymacleod22@gmail.com>
2022-01-14 00:42:13 -0500

    wip: changes some placeholder data

--- src/data.js
+++ src/data.js
@@ -2,6 +2,6 @@ export const boxPlaceHolder = [
     {
         id: 1,
         img: 'https://img.pokemondb.net/sprites/ruby-sapphire/normal/milotic.png',
-        name: 'Milotic'
+        // name: 'Milotic'
     },
 ]
